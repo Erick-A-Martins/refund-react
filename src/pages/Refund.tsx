d5188004e72d39bd293564f3071220dbc77254f2
Erick-A-Martins <erickalvesqmartins@gmail.com>
2026-02-14 13:49:33 -0300

    implement Select behavior and values

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,7 +1,11 @@
+import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
 import { Input } from "../components/Input";
 import { Select } from "../components/Select";
+import { useState } from "react";
 
 export function Refund(){
+    const [category, setCategory] = useState("");
+
     return (
         <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-lg">
             <header>
@@ -15,7 +19,15 @@ export function Refund(){
             </header>
 
             <Input required legend="Nome da solicitação" />
-            <Select required legend="Categoria" />
+            <Select 
+                required 
+                legend="Categoria" 
+                value={category} onChange={(e) => setCategory(e.target.value)}
+            >
+                {CATEGORIES_KEYS.map((category) => (
+                    <option key={category}>{CATEGORIES[category].name}</option>
+                ))}
+            </Select>
         </form>
     )
 }
