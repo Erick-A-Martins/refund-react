3548a25ca8423199de0a4f392423ee26ad537b0c
Erick-A-Martins <erickalvesqmartins@gmail.com>
2026-02-14 11:38:24 -0300

    implement onSubmit behavior using react states

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,24 +1,35 @@
+import { useState } from "react";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 
 export function SignIn(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLoading, setLoading] = useState(false);
+
+    function onSubmit(e: React.SubmitEvent){
+        e.preventDefault();
+        console.log(email, password);
+    }
     return (
-        <form className="w-full flex flex-col gap-4">
+        <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
             <Input 
                 required 
                 legend="E-mail" 
                 type="email" 
                 placeholder="seu@email.com" 
+                onChange={(e) => setEmail(e.target.value)}
             />
 
             <Input 
                 required 
                 legend="Senha" 
-                type="password" 
-                placeholder="123456" 
+                type="password"
+                placeholder="123456"
+                onChange={(e) => setPassword(e.target.value)}
             />
 
-            <Button>Entrar</Button>
+            <Button type="submit" isLoading={isLoading} >Entrar</Button>
         </form>
     );
 }
