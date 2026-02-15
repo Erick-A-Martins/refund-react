adc0ee6760863b9a5a1e204c615c364ee2c5a893
Erick-A-Martins <erickalvesqmartins@gmail.com>
2026-02-15 09:00:13 -0300

    implement Dashboard search bar and Button variants

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,23 @@
+import { classMerge } from "../utils/classMerge"
+
 type Props = React.ComponentProps<"button"> & {
     isLoading?: boolean
+    variant?: "base" | "icon"
+}
+
+const variants = {
+    button: {
+        base: "h-12",
+        icon: "h-12 w-12",
+    }
 }
 
 export function Button({ 
     children, 
-    isLoading, 
-    type= "button", 
+    isLoading,
+    className,
+    type= "button",
+    variant = "base",
     ...rest 
 }: Props){
     return (
@@ -13,7 +25,11 @@ export function Button({
             type={type} 
             disabled={isLoading} 
             {...rest}
-            className="flex items-center justify-center bg-green-100 rounded-lg text-white cursor-pointer hover:bg-green-200 transition ease-linear disabled:opacity-50 disabled:cursor-progress h-12"
+            className={classMerge([
+                "flex items-center justify-center bg-green-100 rounded-lg text-white cursor-pointer hover:bg-green-200 transition ease-linear disabled:opacity-50 disabled:cursor-progress",
+                variants.button[variant],
+                className,
+            ])}
         >
             {children}
         </button>
